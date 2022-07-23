704117519d3b249c1fe44355c6d3f890ff866f17
VitoriaCaminha <vitoria@praciano.com.br>
2022-07-23 16:13:31 -0300

    style: home screen

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2022
+      <a href="https://github.com/VitoriaCaminha" target="_blank">
+        {' '}
+        VitoriaCaminha
+      </a>.
+    </strong>
+  </footer>
+)
