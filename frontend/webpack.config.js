e365e5a09d7438e84988347a19f9565369f0c295
VitoriaCaminha <vitoria@praciano.com.br>
2022-07-23 14:52:14 -0300

    fix: corrected configuration

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/index/jsx',
+  entry: './src/index.jsx',
   output: {
     path: __dirname + '/public',
     filename: './app.js',
@@ -16,7 +16,7 @@ module.exports = {
     alias: {
       modules: __dirname + '/node_modules',
       jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-      bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap;js',
+      bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js',
     },
   },
   plugins: [
@@ -39,7 +39,7 @@ module.exports = {
         },
       },
       {
-        test: /\.css/,
+        test: /\.css$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
       },
       {
