9ee7576526aa09d88c56779195cff3873e7533f9
VitoriaCaminha <vitoria@praciano.com.br>
2022-11-02 18:05:20 -0300

    dashboard layout

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default (props) => (
-  <div>
-    <h1>dashboard</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Dashboard' small='Versão 1.0' />
+        <Content>
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank' value='R$10' text='Total de créditos' />
+            <ValueBox cols='12 4' color='red' icon='credit-card' value='R$10' text='Total de débitos' />
+            <ValueBox cols='12 4' color='blue' icon='money' value='R$0' text='Valor em conta' />
+          </Row>
+        </Content>
+      </div>
+    )
+  }
+}
